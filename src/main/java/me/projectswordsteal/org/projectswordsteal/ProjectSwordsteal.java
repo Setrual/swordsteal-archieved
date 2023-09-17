16f833a76ef44fc56ec8b64ead7461a06046fd59
UpCloudHost <jedi.korn123@gmail.com>
2023-09-17 17:44:44 +0700

    Update: Relocated Events

--- src/main/java/me/projectswordsteal/org/projectswordsteal/ProjectSwordsteal.java
+++ src/main/java/me/projectswordsteal/org/projectswordsteal/ProjectSwordsteal.java
@@ -3,8 +3,11 @@ package me.projectswordsteal.org.projectswordsteal;
 import me.projectswordsteal.org.projectswordsteal.listeners.events.test_events;
 import me.projectswordsteal.org.projectswordsteal.listeners.test_listeners;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import static me.projectswordsteal.org.projectswordsteal.listeners.message.cmd_logs.CompleteLog;
@@ -21,6 +24,20 @@ public final class ProjectSwordsteal extends JavaPlugin implements Listener {
         Bukkit.getPluginManager().registerEvents(new test_listeners(), this);
         CompleteLog();
 
+
+    }
+
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent e) {
+        e.setCancelled(true);
+        e.getPlayer().sendMessage(ChatColor.RED + "You cannot move right now!");
+
+    }
+
+    @EventHandler
+    public void onPlayerEggThrow(PlayerEggThrowEvent e) {
+        e.getPlayer().sendMessage(ChatColor.BLUE + "You just threw an egg!");
     }
 
     @Override
--- src/main/java/me/projectswordsteal/org/projectswordsteal/listeners/main_listeners.java
+++ src/main/java/me/projectswordsteal/org/projectswordsteal/listeners/main_listeners.java
@@ -7,15 +7,5 @@ import org.bukkit.event.player.PlayerEggThrowEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 public class main_listeners implements Listener {
-    @EventHandler
-    public void onPlayerMove(PlayerMoveEvent e) {
-        e.setCancelled(true);
-        e.getPlayer().sendMessage(ChatColor.RED + "You cannot move right now!");
 
-    }
-
-    @EventHandler
-    public void onPlayerEggThrow(PlayerEggThrowEvent e) {
-        e.getPlayer().sendMessage(ChatColor.BLUE + "You just threw an egg!");
-    }
 }
